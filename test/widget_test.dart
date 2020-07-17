de4674aef2e0a5cd664412f4ed27cda54a214a95
Pranav Bedre <bedrepranav@gmail.com>
2020-07-17 17:52:34 +0530

    test: Update widget testing to fix #5

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -5,15 +5,17 @@
 // gestures. You can also use WidgetTester to find child widgets in the widget
 // tree, read text, and verify that the values of widget properties are correct.
 
-// import 'package:flutter/material.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:incodnito/ui/pages/incodnito.dart';
+import 'package:incodnito/ui/pages/landing.dart';
 
 void main() {
   testWidgets('Incodnito title test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(Incodnito());
+    await tester.pumpWidget(MaterialApp(
+      home: Landing(),
+    ));
 
     final titleFinder = find.text('Incodnito');
 
