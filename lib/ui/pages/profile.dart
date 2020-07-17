bc8871f9ea9a238ba970a9f2a4afe12e941522a1
Prahasith <50479071+YPrahasith@users.noreply.github.com>
2020-07-17 15:30:15 +0530

    profile: Add profile pic, edit button & karma points

--- lib/ui/pages/profile.dart
+++ lib/ui/pages/profile.dart
@@ -21,43 +21,49 @@ class _ProfileState extends State<Profile> {
         color: Colors.black,
         child: Padding(
             padding: const EdgeInsets.all(5.0),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              mainAxisSize: MainAxisSize.max,
-              mainAxisAlignment: MainAxisAlignment.start,
-              children: [
-                Image.asset(
-                  'assets/images/dp.jpeg',
-                  width: 100,
-                  height: 100,
-                ),
-                SizedBox(
-                  height: _height * 0.009,
-                ),
-                RaisedButton(
-                  color: Colors.grey,
-                  textColor: Colors.black,
-                  onPressed: () {},
-                  shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(30.0)),
-                  child: Text("Edit Profile"),
-                ),
-                SizedBox(
-                  height: _height * 0.009,
-                ),
-                Text(
-                  "Your Points",
-                  style: TextStyle(fontSize: 22, color: Colors.grey),
-                ),
-                SizedBox(
-                  height: _height * 0.009,
-                ),
-                Text(
-                  "Karma Symbol- 69",
-                  style: TextStyle(fontSize: 16, color: Colors.grey),
-                ),
-              ],
-            )),
+          child : Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            mainAxisSize: MainAxisSize.max,
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: [
+              Image.asset(
+                'assets/images/dp.jpeg',
+                width: 100,
+                height: 100,
+              ),
+              
+              SizedBox(
+                height:_height*0.009,
+              ),
+              
+              RaisedButton(
+                color : Colors.grey,
+                textColor: Colors.black,
+                onPressed: () {},
+                shape:RoundedRectangleBorder(borderRadius: BorderRadius.circular(30.0)),
+                child: Text("Edit Profile"),
+              ),
+              
+              SizedBox(
+                height:_height*0.009,
+              ),
+              
+              Text(
+                "Your Points" ,
+                style: TextStyle(fontSize:22, color:Colors.grey),
+              ),
+              
+              SizedBox(
+                height:_height*0.009,
+              ),
+              
+              Text( 
+                "Karma Symbol- 69",
+                style: TextStyle(fontSize:16, color:Colors.grey),
+              ),
+            ],
+            
+          )
       ),
     );
   }
